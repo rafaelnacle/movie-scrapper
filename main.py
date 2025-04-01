07de97d8d758013ba67952f51e75dc30c3cff0d1
= <=>
2025-04-01 15:09:34 -0300

    make the movies write into a txt files

--- main.py
+++ main.py
@@ -11,4 +11,8 @@ soup = BeautifulSoup(website_html, "html.parser")
 all_movies = soup.find_all(name="h3", class_="title")
 
 movie_titles = [movie.get_text() for movie in all_movies]
-print(movie_titles[::-1])
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w", encoding="utf-8") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
